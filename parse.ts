7ed5ca60835ea3f22d9d84f194288b3c87e4b79f
Nate Roling <roling@cartegraph.com>
2022-10-01 12:42:51 -0500

    Cleanup

--- parse.ts
+++ parse.ts
@@ -1,78 +1,50 @@
 import * as P from "parsimmon";
 
-// Operator definitions
-const binary = ["is equal to", "is not equal to"];
-const unary = ["is null"];
-const dateOperators = ["is before"];
-const numericOperators = ["<", ">"];
+// Utilities
+const oneOfString = (strs: string[]) =>
+  P.alt(...strs.map(P.string)).trim(P.optWhitespace);
 
 // Atoms
-const number = P.regexp(/[0-9.]+/); // TODO parsing a number is probably more complex
-const text = P.regexp(/[a-z ,A-Z0-9\\\._:-]*/); // TODO parsing a quoted string is probably a bit more complex than this
+const number = P.regexp(/[0-9.]+/); // TODO probably too simple
+const text = P.regexp(/[a-z ,A-Z0-9\\\._:-]*/); // TODO probably too simple
+
+// Operator definitions
+// Operators include any whitespace around them
+const unaryOp = oneOfString(["is null"]);
+const binaryOp = oneOfString(["is equal to", "is not equal to"]);
+const numOp = oneOfString(["<", ">"]);
+const dateOp = oneOfString(["is before"]);
+const condOp = oneOfString(["AND", "OR"]);
+
 const field = text.wrap(P.string("["), P.string("]"));
 const quoted = text.wrap(P.string('"'), P.string('"'));
 const date = text.wrap(P.string("#"), P.string("#"));
 
-const op = P.alt(...binary.map(P.string));
-const unaryOp = P.alt(...unary.map(P.string));
-const dateOp = P.alt(...dateOperators.map(P.string));
-const numOp = P.alt(...numericOperators.map(P.string));
+const unaryClause = P.seq(field, unaryOp);
+const binaryClause = P.seq(field, binaryOp, quoted);
+const numClause = P.seq(field, numOp, number);
+const dateClause = P.seq(field, dateOp, date);
 
-const stringClause = P.seq(
-  field.skip(P.whitespace),
-  op.skip(P.whitespace),
-  quoted
-);
-
-const unaryClause = P.seq(field.skip(P.whitespace), unaryOp);
-
-const dateClause = P.seq(
-  field.skip(P.whitespace),
-  dateOp.skip(P.whitespace),
-  date
-);
-
-const numClause = P.seq(
-  field.skip(P.whitespace),
-  numOp.skip(P.whitespace),
-  number
-);
-
-const condOp = P.alt(
-  P.string("AND").trim(P.optWhitespace),
-  P.string("OR").trim(P.optWhitespace)
+const clause = P.alt(unaryClause, binaryClause, numClause, dateClause).map(
+  ([lhs, op, rhs]) => ({ lhs, op, rhs })
 );
 
 // UTILS
 const unwrapParens = <T>(p: P.Parser<T>): P.Parser<T> => {
-  const unwrapRecur = P.lazy(() =>
-    P.string("(").then(unwrapRecur.or(p)).skip(P.string(")"))
+  const recur = P.lazy(() =>
+    P.string("(").then(recur.or(p)).skip(P.string(")"))
   );
-  return unwrapRecur;
+  return recur;
 };
 
-const clause = P.alt(stringClause, dateClause, numClause, unaryClause).map(
-  ([lhs, op, rhs]) => ({
-    lhs,
-    op,
-    rhs,
-  })
-);
-const countExpr = unwrapParens(
-  P.seq(
-    P.string("Count"),
-    unwrapParens(clause).skip(P.whitespace),
-    numOp.skip(P.whitespace),
-    number
-  ).map(([_, c, o, v]) => ({
-    type: "Count",
-    clause: c,
-    op: o,
-    rhs: v,
-  }))
-);
+const countExpr = P.seq(
+  P.string("Count"),
+  unwrapParens(clause),
+  numOp,
+  number
+).map(([_, c, o, v]) => ({ type: "Count", clause: c, op: o, rhs: v }));
 
-const group: P.Parser<any> = countExpr.or(unwrapParens(clause));
+const group = unwrapParens(P.alt(countExpr, clause));
 
 const conditional = P.lazy(() => {
   return P.seq(
