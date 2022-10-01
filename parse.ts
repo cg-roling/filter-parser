41b6d7b1100051cfe65a4cf77487dd6dcec930a7
Nate Roling <roling@cartegraph.com>
2022-10-01 18:33:21 -0500

    Clean clean clean

--- parse.ts
+++ parse.ts
@@ -1,6 +1,6 @@
 import * as P from "parsimmon";
 
-// An operator, ignoring leading/trailing whitespace
+// An operator, discarding leading/trailing whitespace
 const operator = (strs: string[]) =>
   P.alt(...strs.map(P.string)).trim(P.optWhitespace);
 
@@ -19,46 +19,42 @@ export const optParens = <T>(p: P.Parser<T>): P.Parser<T> => p.or(parens(p));
 // Atoms
 const number = P.regexp(/[0-9.]+/); // TODO probably too simple
 const text = P.regexp(/[a-z ,A-Z0-9\\\._:-]*/); // TODO probably too simple
+const field = text.wrap(P.string("["), P.string("]"));
+const quoted = text.wrap(P.string('"'), P.string('"'));
+const date = text.wrap(P.string("#"), P.string("#"));
 
-// Operator definitions include any whitespace around them
+// Operator definitions discard any whitespace around them
 const unaryOp = operator(["is null"]);
 const binaryOp = operator(["is equal to", "is not equal to"]);
 const numOp = operator(["<", ">"]);
 const dateOp = operator(["is before"]);
 const condOp = operator(["AND", "OR"]);
 
-const field = text.wrap(P.string("["), P.string("]"));
-const quoted = text.wrap(P.string('"'), P.string('"'));
-const date = text.wrap(P.string("#"), P.string("#"));
-
-const unaryClause = P.seq(field, unaryOp);
-const binaryClause = P.seq(field, binaryOp, quoted);
-const numClause = P.seq(field, numOp, number);
-const dateClause = P.seq(field, dateOp, date);
+// Comparisons
+const unaryCompare = P.seq(field, unaryOp);
+const binaryCompare = P.seq(field, binaryOp, quoted);
+const numCompare = P.seq(field, numOp, number);
+const dateCompare = P.seq(field, dateOp, date);
 
 const lang = P.createLanguage({
-  clause: (r) =>
-    P.alt(unaryClause, binaryClause, numClause, dateClause).map(
+  expression: (r) => optParens(P.alt(r.andOr, r.count, r.compare)),
+
+  compare: (r) =>
+    P.alt(unaryCompare, binaryCompare, numCompare, dateCompare).map(
       ([lhs, op, rhs]) => ({ lhs, op, rhs })
     ),
-  conditionalChild: (r) =>
-    parens(P.alt(r.countExpr, r.clause, r.conditionalExpr)),
 
-  conditionalExpr: (r) =>
-    P.seq(
-      r.conditionalChild,
-      condOp,
-      r.conditionalExpr.or(r.conditionalChild)
-    ).map(([lhs, op, rhs]) => ({ lhs, op, rhs })),
-
-  countExpr: (r) =>
+  count: (r) =>
     P.string("Count")
       .then(P.seq(r.expression, numOp, number))
       .map(([lhs, op, rhs]) => ({ type: "Count", lhs, op, rhs })),
 
-  expression: (r) => {
-    return optParens(P.alt(r.conditionalExpr, r.countExpr, r.clause));
-  },
+  andOr: (r) =>
+    P.seq(parens(r.expression), condOp, r.expression).map(([lhs, op, rhs]) => ({
+      lhs,
+      op,
+      rhs,
+    })),
 });
 
 export const parseFilter = (filterString: string) => {
